95a3100c210bf2142a7e8b9af094e4cdf2bac976
kaluginadaria <kaluginadaria977@yandex.ru>
2018-07-23 20:37:59 -0400

    6HW map

---
+++ test/ru/javawebinar/mybase/storage/MapResumeStorageTest.java
@@ -0,0 +1,8 @@
+package ru.javawebinar.mybase.storage;
+
+public class MapResumeStorageTest extends AbstractStorageTest {
+
+    public MapResumeStorageTest() {
+        super(new MapResumeStorage());
+    }
+}
