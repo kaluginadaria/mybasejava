cf05699f8e473aa7caa047b9b4920dd6824eb761
kaluginadaria <kaluginadaria977@yandex.ru>
2018-07-30 11:19:16 -0400

    10 lesson data input/output stream

---
+++ test/ru/javawebinar/mybase/storage/DataPathStorageTest.java
@@ -0,0 +1,9 @@
+package ru.javawebinar.mybase.storage;
+
+import ru.javawebinar.mybase.storage.serializer.DataStreamSerializer;
+
+public class DataPathStorageTest extends AbstractStorageTest {
+    public DataPathStorageTest() {
+        super(new PathStorage(STORAGE_DIR.getAbsolutePath(), new DataStreamSerializer()));
+    }
+}
